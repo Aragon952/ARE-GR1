8f028e918ab4bf306f7edfa432723a755944cb6a
Mihai <petrovmihai952@gmail.com>
2025-11-05 19:34:50 +0200

    tested variables

---
+++ test.js
@@ -0,0 +1,30 @@
+
+
+// function testVariables(param){
+//     if (param == true){
+//         var x = 5;
+//         let y = 6;
+//         const z =7;
+//     }
+//     console.log(x);
+//     console.log(y);
+//     console.log(z);
+// }
+
+// testVariables(true)
+
+
+const testVariables2 = (param) => {
+    console.log(x)
+    if (param === true){
+        var x = 5;
+        let y = 6;
+        const z =7;
+    }
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+
+testVariables2(true);
+
